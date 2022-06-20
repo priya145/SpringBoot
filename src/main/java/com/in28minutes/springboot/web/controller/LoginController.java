ff4523c1fe6b9605c9e42ba3171bfcde2e2907e9
priya145 <tayadepriya31@gmail.com>
2022-06-20 12:26:39 +0530

    Added Webadpp and configured View code

--- src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -1,18 +1,27 @@
 package com.in28minutes.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 // /login => 'Hello World'
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
-	@ResponseBody
-	public String loginMessage() {
-		return "Hello World";
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String showLoginMessage(ModelMap model) {
+		//model.put("name", name);
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String showWelcomeMessage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		return "home";
 	}
 
 }
